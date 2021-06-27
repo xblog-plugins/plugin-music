6ee4040f0c88f17fc6f84d5c23a0a1a2630de26b
xiaoyou66 <xiaoyou2333@foxmail.com>
2021-06-27 12:21:50 +0800

    prefect: 完善音乐播放器

--- index.js
+++ index.js
@@ -2,16 +2,33 @@
 const widget =xBlog.widget
 const database =xBlog.database
 const tools =xBlog.tools
+const router = xBlog.router
+const net = xBlog.net
 
 // 常量
 const keyMusicContent = 'music_content'
 
+// 获取音乐歌词
+router.registerRouter("GET","/:id/irc",function(context){
+    net.get("http://music.163.com/api/song/media?id="+context.Param("id"),{"Host":"music.163.com"},function (err,res){
+        if (err==null){
+            let data = JSON.parse(res)
+            if (data.lyric!==undefined){
+                router.response.ResponseHtml(context,data.lyric)
+                return
+            }
+        }
+        router.response.ResponseHtml(context,"")
+    })
+})
+
+
 // 添加卡片
 widget.addSide("","index.html",function () {
-    let content = tools.getSetting(keyMusicContent)
-    tools.log(JSON.stringify(content))
     // 初始化数据库链接
-    return {}
+    return {
+        content: JSON.stringify(tools.getSetting(keyMusicContent))
+    }
 },true)
 
 
